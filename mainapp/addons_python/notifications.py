944b40ab388fbc919974a1d6097a8d24ea4eddee
mnklop <maxim210204@yandex.ru>
2021-10-02 00:41:22 +0300

    Отправка оповещений переделана на django.mail

--- mainapp/addons_python/notifications.py
+++ mainapp/addons_python/notifications.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-"""Send email via smtp_host."""
-
-import smtplib
-from email.header import Header
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import requests
+from django.core.mail import EmailMultiAlternatives
 
-smtp_host = "mail.hosting.reg.ru"
 login = "admin@it-class1158.site"
-password = "wsx123451"
+
 
 # функция отправки сообщений на почту
-def send_mail_to_applicant(theme_letter, header_letter, text_letter, recipients_email):
+def send_mail_to_applicant(theme_letter, header_letter, text_letter,
+                           recipients_email):
     for email in recipients_email:
-        html = f"""\
+        html_content = f"""\
         <html>
           <head></head>
           <body>
-        <img src="https://it-class1158.site/static/images/itclass-logo.png" width="150" height="150">
+        <img src="https://it-class1158.ru/itclass/static/images/logo_it_class-5.png" 
+        width="150" height="100">
             <h1 style="text-align: center;">{header_letter}</h1><br>
                <p style="font-size: 18px;">{text_letter}</p>
         <hr>
@@ -28,34 +24,26 @@ def send_mail_to_applicant(theme_letter, header_letter, text_letter, recipients_
           </body>
         </html>
         """
-        msg = MIMEMultipart()
-        msg['Subject'] = Header(f"{theme_letter}", 'utf-8')
-        msg['From'] = login
-        msg['To'] = email
-        part = MIMEText(html, 'html')
-        msg.attach(part)
-        s = smtplib.SMTP(smtp_host, 587)
-        s.set_debuglevel(1)
-        try:
-            s.starttls()
-            s.login(login, password)
-            s.sendmail(login, recipients_email, msg.as_string())
-        finally:
-            print(msg)
-            s.quit()
+        subject, from_email, to = theme_letter, login, email
+        text_content = text_letter
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+
 # функция отправки сообщений в телеграмм канал {"ok":true,"result":{"message_id":3,"chat":{"id":-1001380508914,"title":"\u0418\u0442-\u043a\u043b\u0430\u0441\u0441","username":"it_class1158","type":"channel"},"date":1595839792,"text":"test"}}
 def send_telegram(text: str):
     token = "1098094286:AAGmN7YpguBfJOiPgXLyx-OtPW15pQ76ESU"
     url = "https://api.telegram.org/bot"
-    #channel_id = "-1001380508914" - основной канал
-    channel_id = -1001134619840 #тестовый канал
+    # channel_id = "-1001380508914" - основной канал
+    channel_id = -1001134619840  # тестовый канал
     url += token
     method = url + "/sendMessage"
 
     r = requests.post(method, data={
-         "chat_id": channel_id,
-         "text": text
-          })
+        "chat_id": channel_id,
+        "text": text
+    })
 
     if r.status_code != 200:
         raise Exception("post_text error")
